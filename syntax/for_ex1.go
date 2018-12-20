2ed7a5410d05406cd99dcdd6cb1f7714e698c23b
Shogo Nagasaka <nagasaka.shogo@donuts.ne.jp>
2018-12-20 18:25:51 +0900

    add files

---
+++ syntax/for_ex1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 0 から 99 までの数字を順に追加したスライスを作成する
+	nums := make([]int, 0, 100)
+
+	fmt.Printf("nums = %v\n", nums)
+
+	// 最後の要素から逆順にスライスの内容を出力する
+}
---
+++ syntax/func_ex1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Sum(10) = %d\n", Sum(10))
+}
+
+// 0 から n までの数字の和を計算する関数を作成する
+func Sum(n int) int {
+	return 0
+}
